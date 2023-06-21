bedf3d827ab2625173b19e99a01a231740ef3182
grant.lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2023-06-21 14:32:39 +0100

    scratch

---
+++ Neo4j.Driver/Scratch/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Linq.Expressions;
+
+Expression<Func<int, bool>> expr = i => i < 5;
+
+
+Console.WriteLine(expr.Name);
+Console.WriteLine(expr.Type);
+Console.WriteLine(expr.Parameters);
+Console.WriteLine(expr.NodeType);
+Console.WriteLine(expr.TailCall);
+Console.WriteLine(expr.Body);
+Console.WriteLine();
+Console.WriteLine(expr.Body.NodeType);
+Console.WriteLine(expr.Body.Type);
